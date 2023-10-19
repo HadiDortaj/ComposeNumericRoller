338c8fe47165f407f3cb892cd06469e5a1d039f2
Hadi Dortaj <hadi.dortaj@tapsi.cab>
2023-10-19 14:57:15 +0330

    Add Support for multiple characters

--- app/src/main/java/com/hadi/animatingprice/MainActivity.kt
+++ app/src/main/java/com/hadi/animatingprice/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.hadi.animatingprice
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,12 +12,17 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hadi.animatingprice.ui.theme.AnimatingPriceTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +30,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             AnimatingPriceTheme {
                 // A surface container using the 'background' color from the theme
-                Greeting()
+                var price by remember {
+                    mutableStateOf(23)
+                }
+
+                LaunchedEffect(key1 = Unit) {
+                    delay(3000)
+
+                    price = 93
+
+                    delay(3000)
+                    price = 423
+
+                }
+                Greeting(price)
             }
         }
     }
@@ -41,30 +60,71 @@ steps:
  */
 
 @Composable
-fun Greeting() {
-    val items = listOf(",", *(0..10).map { it.toString() }.toTypedArray())
+fun Greeting(price: Int) {
 
-    val lazyListState = rememberLazyListState(initialFirstVisibleItemIndex = 0)
-    LaunchedEffect(key1 = Unit) {
-        lazyListState.animateScrollToItem(5)
-    }
-    Box(
+    val priceCharacters = generateCharactersArray(price)
+
+    Row(
         modifier = Modifier
             .clipToBounds()
             .height(50.dp)
     ) {
-        LazyColumn(state = lazyListState) {
-            items(items = items) { item ->
-                Text(text = item, fontSize = 40.sp, modifier = Modifier.height(50.dp))
+        priceCharacters.forEachIndexed { index, character ->
+            key(index, character) {
+                if (character != null) {
+                    Log.i("HADI", "index: $index $character")
+                    CharacterItem(character)
+                }
             }
+        }
+
+    }
+}
+
+@Composable
+private fun CharacterItem(character: Char) {
+    val lazyListState = rememberLazyListState(initialFirstVisibleItemIndex = 0)
+    LaunchedEffect(key1 = character) {
+        lazyListState.animateScrollToItem(getCharacterIndex(character))
+    }
+    LazyColumn(state = lazyListState) {
+        items(items = getItems()) { item ->
+            Text(text = item, fontSize = 40.sp, modifier = Modifier.height(50.dp))
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AnimatingPriceTheme {
-        Greeting()
+private fun generateCharactersArray(price: Int): Array<Char?> {
+    val charactersArray = price.toString().toCharArray()
+    val supportedCharacters = 10
+    val modifiedArray = Array(supportedCharacters) { index ->
+        val sizeDiff = supportedCharacters - charactersArray.size
+        if (index >= sizeDiff) {
+            charactersArray[index - sizeDiff]
+        } else {
+            null
+        }
+    }
+    return modifiedArray
+}
+
+fun getItems(): List<String> {
+    return listOf(*(0..9).map { it.toString() }.toTypedArray())
+}
+
+fun getCharacterIndex(character: Char): Int {
+    return if (character.isDigit()) {
+        return character.digitToInt()
+    } else {
+        0
     }
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    AnimatingPriceTheme {
+//        Greeting(4)
+//    }
+//}
