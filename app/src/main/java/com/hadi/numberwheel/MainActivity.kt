142b658fad3a9bf497dbb718227bb519ec3ade62
Hadi Dortaj <hadi.dortaj@tapsi.cab>
2023-10-20 16:57:20 +0330

    Add MainScree composable

--- app/src/main/java/com/hadi/numberwheel/MainActivity.kt
+++ app/src/main/java/com/hadi/numberwheel/MainActivity.kt
@@ -3,30 +3,14 @@ package com.hadi.numberwheel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import com.hadi.numberwheel.ui.theme.NumberWheelTheme
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NumberWheelTheme {
-                var price by remember {
-                    mutableStateOf(2)
-                }
-                LaunchedEffect(key1 = Unit) {
-                    val changes = listOf(12)
-                    changes.forEach {
-                        delay(3000)
-                        price = it
-                    }
-                }
-                NumberWheel(price)
+                MainScreen()
             }
         }
     }
