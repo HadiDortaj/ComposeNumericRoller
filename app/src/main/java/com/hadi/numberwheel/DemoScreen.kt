a52391402553f385aef9860487bce392c8d11856
Hadi Dortaj <hadi.dortaj@tapsi.cab>
2023-10-20 18:34:47 +0330

    Add simple theme and typeface

---
+++ app/src/main/java/com/hadi/numberwheel/DemoScreen.kt
@@ -0,0 +1,46 @@
+package com.hadi.numberwheel
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.delay
+
+@Composable
+fun DemoScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xffF1EBE3))
+    ) {
+        var number by remember {
+            mutableStateOf(123456)
+        }
+        LaunchedEffect(key1 = Unit) {
+            val changes = listOf(654321, 123456, 123458, 123398, 423398, 411398, 123456)
+            delay(1000)
+            while (true) {
+                changes.forEach {
+                    number = it
+                    delay(2000)
+                }
+            }
+        }
+        NumberWheel(number = number, modifier = Modifier.align(Alignment.Center))
+    }
+}
+
+@Preview
+@Composable
+private fun MainScreenPreview() {
+    DemoScreen()
+}
--- app/src/main/java/com/hadi/numberwheel/DigitWheel.kt
+++ app/src/main/java/com/hadi/numberwheel/DigitWheel.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.TweenSpec
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -12,14 +13,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 private const val EMPTY_ITEM_INDEX = 0
-private const val ANIMATION_DURATION_IN_MILLIS = 1000
-private val DIGIT_HEIGHT = 50.dp
+private const val ANIMATION_DURATION_IN_MILLIS = 2000
+private val DIGIT_HEIGHT = 80.dp
 
 @Composable
 fun DigitWheel(digit: Char, canAnimate: Boolean, modifier: Modifier = Modifier) {
@@ -50,8 +56,17 @@ fun DigitWheel(digit: Char, canAnimate: Boolean, modifier: Modifier = Modifier) 
     ) {
         items(items = getWheelItems()) { item ->
             Text(
-                text = item, fontSize = 40.sp,
-                modifier = Modifier.height(DIGIT_HEIGHT)
+                text = item, fontSize = 60.sp,
+                modifier = Modifier.height(DIGIT_HEIGHT).width(50.dp),
+                color = Color(0xffBC9E4C),
+                style = TextStyle(
+                    fontFamily = FontFamily(
+                        Font(
+                            R.font.libre_baskerville_regular,
+                            FontWeight.Normal
+                        )
+                    )
+                )
             )
         }
     }
